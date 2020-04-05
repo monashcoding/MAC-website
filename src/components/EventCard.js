c4cb1642ea9e682c98a5c03e9de2b5ecb27d5bac
khanguslee <khanguslee@gmail.com>
2020-04-05 15:31:03 +1000

    Create Tag component

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Tag from '../components/Tag';
+
 function EventCard(props) {
     const { title, image, date, time } = props;
 
     return (
         <div className="event-card w-full md:w-2/5 xl:1/3 m-4 h-50 rounded overflow-hidden shadow-lg hover:shadow-xl cursor-pointer">
             <div className="image-container relative">
-                {(date || time) && <span className="bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 absolute float-right top-1 right-1">{`${date} ${time}`}</span>}
+                {(date || time) && <Tag text={`${date} ${time}`} className={"absolute float-right top-1 right-1"} />}
                 {image ? <img className="w-full h-48 md:h-56 object-cover" src={image} ></img> : <div className="h-48 md:h-56"></div>}
             </div>
             <div className="font-bold mx-2 pb-2">{title}</div>
