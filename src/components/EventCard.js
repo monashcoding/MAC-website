00e51bb1722ee4c6905761cb2692c9751b22354d
khanguslee <khanguslee@gmail.com>
2020-04-05 15:37:00 +1000

    Add alt title to image in event card component

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -10,7 +10,7 @@ function EventCard(props) {
         <div className="event-card w-full md:w-2/5 xl:1/3 m-4 h-50 rounded overflow-hidden shadow-lg hover:shadow-xl cursor-pointer">
             <div className="image-container relative">
                 {(date || time) && <Tag text={`${date} ${time}`} className={"absolute float-right top-1 right-1"} />}
-                {image ? <img className="w-full h-48 md:h-56 object-cover" src={image} ></img> : <div className="h-48 md:h-56"></div>}
+                {image ? <img className="w-full h-48 md:h-56 object-cover" src={image} alt={title}></img> : <div className="h-48 md:h-56"></div>}
             </div>
             <div className="font-bold mx-2 pb-2">{title}</div>
         </div>
