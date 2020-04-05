aeb9d131a234039e473f21b13a2585b0c8bf58ed
khanguslee <khanguslee@gmail.com>
2020-04-05 15:21:52 +1000

    Add proptypes to EventCard component

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -7,7 +7,7 @@ function EventCard(props) {
     return (
         <div className="event-card w-full md:w-2/5 xl:1/3 m-4 h-50 rounded overflow-hidden shadow-lg hover:shadow-xl cursor-pointer">
             <div className="image-container relative">
-                <span className="bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 absolute float-right top-1 right-1">{`${date} ${time}`}</span>
+                {(date || time) && <span className="bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 absolute float-right top-1 right-1">{`${date} ${time}`}</span>}
                 {image ? <img className="w-full h-48 md:h-56 object-cover" src={image} ></img> : <div className="h-48 md:h-56"></div>}
             </div>
             <div className="font-bold mx-2 pb-2">{title}</div>
@@ -16,11 +16,17 @@ function EventCard(props) {
 }
 
 EventCard.propTypes = {
-    title: PropTypes.string
+    title: PropTypes.string,
+    image: PropTypes.string,
+    date: PropTypes.string,
+    time: PropTypes.string,
 }
 
 EventCard.defaultProps = {
-    title: "No Title"
+    title: "No Title",
+    image: "",
+    date: "",
+    time: "",
 }
 
 export default EventCard;
