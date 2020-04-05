8bc44b0e430e027e319d74802911f3d555a1baef
khanguslee <khanguslee@gmail.com>
2020-04-05 17:21:52 +1000

    Sort events to past and upcoming events

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment';
 
 import useEventPosts from '../hooks/useEventPosts';
 
@@ -8,10 +9,14 @@ import EventSection from '../components/EventSection';
 import '../styles/styles.scss';
 
 function IndexPage() {
-  const upcomingEvents = [];
+  const events = useEventPosts();
 
-  // TODO: Sort data into past and upcoming
-  const pastEvents = useEventPosts();
+  // Filter out events by todays date
+  const eventIndex = events.findIndex(item => {
+    return moment(item.eventDate).isBefore();
+  });
+  const upcomingEvents = eventIndex >= 0 ? events.slice(0, eventIndex) : [];
+  const pastEvents = eventIndex >= 0 ? events.slice(eventIndex) : [];
 
   return (
     <div>
