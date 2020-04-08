1c945429974bd2d959478559fae2bbb7842b748d
Nicholas Chong <nchong128@gmail.com>
2020-04-09 01:00:25 +1000

    More styling changes

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -42,24 +42,26 @@ function EventCard(props) {
         contentLabel="Example Modal"
       >
       <div>
-        <div className="relative max-w-full bg-white px-48 py-8 border-b">
+        <div className="relative max-w-full bg-white px-32 py-8 border-b">
           <div className="max-w-screen-m">
             {(date || time) && (
               <Tag
                 text={`${date}`}
               />
             )}
-            <div className="text-3xl text-gray-900 font-bold mt-4 mx-2 pb-2">{title}</div>
-            <btn
-              className="px-5 py-2 border-solid border-gray-500 rounded-lg border absolute top-0 right-1"
-            >
-              <span className="text-gray-700 font-semibold">Share</span>
-            </btn>
+            <div className="relative">
+              <div className="text-3xl text-gray-900 font-bold mt-4 mx-2 pb-2">{title}</div>
+              <btn
+                className="px-5 py-2 border-solid border-gray-500 rounded-lg border absolute top-0 right-1 cursor-pointer"
+              >
+                <span className="text-gray-700 font-semibold">Share</span>
+              </btn>
+            </div>
           </div>
 
         </div>
 
-        <div className="relative max-w-full px-48 mx-auto mt-8">
+        <div className="relative max-w-full px-32 mx-auto mt-8">
           {image ? (
             <img
               className="w-7/12 h-full md:h-56 mt-8 mx-2 object-cover"
@@ -72,7 +74,8 @@ function EventCard(props) {
           {description ? (
             <>
             <div className="text-xl text-gray-900 font-bold mx-2 mb-2 mt-8">Details</div>
-            <div className="mt-4 mx-2 w-7/12 text-gray-900">{description}</div>
+            {/* <div className="mt-4 mx-2 w-7/12 whitespace-pre-wrap"><pre className="text-gray-900 font-sans w-7/12">{description}</pre></div> */}
+            <div className="mt-4 mx-2 w-7/12 whitespace-pre-wrap">{description}</div>
             </>
           ) : (
             <div className="text-l text-gray-900 mx-2">No description yet!</div>
