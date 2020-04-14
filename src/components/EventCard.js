77453515e173816682e26a25c4e700ac399beaeb
Nicholas Chong <nchong128@gmail.com>
2020-04-14 20:00:25 +1000

    Extract modal to separate file

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -10,15 +10,6 @@ const modalStyles = {
   content: {
     backgroundColor: "#f6f7f8",
     padding: 0
-  //   position: "relative",
-  //   top: "auto",
-  //   left: "auto",
-  //   right: "auto",
-  //   bottom: "auto",
-  //   maxWidth: "1620px",
-  //   margin: "32px auto",
-  //   padding: 10,
-  //   border: 0
   }
 };
 
@@ -28,65 +19,22 @@ function EventCard(props) {
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
   const openModal = () => setModalIsOpen(true);
-  const closeModal = () => setModalIsOpen(false);
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
 
   return (
     <div onClick={openModal} className="event-card w-full md:w-2/5 xl:1/3 m-4 h-50 rounded overflow-hidden shadow-lg hover:shadow-xl cursor-pointer">
-      {/* TODO: extract modal into its own component  */}
-      <Modal
-        isOpen={modalIsOpen}
-        // onAfterOpen={afterOpenModal}
-        onRequestClose={closeModal}
-        shouldCloseOnOverlayClick={true}
-        style={modalStyles}
-        contentLabel="Example Modal"
-      >
-      <div>
-        <div className="relative max-w-full bg-white px-32 py-8 border-b">
-          <div className="max-w-screen-m">
-            {(date || time) && (
-              <Tag
-                text={`${date}`}
-              />
-            )}
-            <div className="relative">
-              <div className="text-3xl text-gray-900 font-bold mt-4 mx-2 pb-2">{title}</div>
-              <btn
-                className="px-5 py-2 border-solid border-gray-500 rounded-lg border absolute top-0 right-1 cursor-pointer"
-              >
-                <span className="text-gray-700 font-semibold">Share</span>
-              </btn>
-            </div>
-          </div>
+      <EventModal 
+        // key={index}
+        title={event.title}
+        image={event.image}
+        description={event.description}
+        date={eventDate}
+        time={eventTime}
+      />
 
-        </div>
-
-        <div className="relative max-w-full px-32 mx-auto mt-8">
-          {image ? (
-            <img
-              className="w-7/12 h-full md:h-56 mt-8 mx-2 object-cover"
-              src={image}
-              alt={title}
-            ></img>
-          ) : (
-            <div></div>
-          )}
-          {description ? (
-            <>
-            <div className="text-xl text-gray-900 font-bold mx-2 mb-2 mt-8">Details</div>
-            {/* <div className="mt-4 mx-2 w-7/12 whitespace-pre-wrap"><pre className="text-gray-900 font-sans w-7/12">{description}</pre></div> */}
-            <div className="mt-4 mx-2 w-7/12 whitespace-pre-wrap">{description}</div>
-            </>
-          ) : (
-            <div className="text-l text-gray-900 mx-2">No description yet!</div>
-          )}
-        </div>
-        
-
-
-      </div>
-      </Modal>
-    
       <div className="image-container relative">
         {(date || time) && (
           <Tag
@@ -107,7 +55,7 @@ function EventCard(props) {
       <div className="font-bold mx-2 pb-2">{title}</div>
     </div>
   );
-}
+};
 
 EventCard.propTypes = {
   title: PropTypes.string,
--- src/components/EventModal.js
+++ src/components/EventModal.js
@@ -1,2 +1,60 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
+function EventModal(props) {
+  const {title,image,description,date,time} = props;
+
+  return (
+    <Modal
+      isOpen={modalIsOpen}
+      onRequestClose={() => setModalIsOpen(false)}
+      shouldCloseOnOverlayClick={true}
+      style={modalStyles}
+      contentLabel="Example Modal"
+    >
+    <div>
+        <div className="relative max-w-full bg-white px-32 py-8 border-b">
+        <div className="max-w-screen-m">
+            {(date || time) && (
+            <Tag
+                text={`${date}`}
+            />
+            )}
+            <div className="relative">
+            <div className="text-3xl text-gray-900 font-bold mt-4 mx-2 pb-2">{title}</div>
+            <button
+                className="px-5 py-2 border-solid border-gray-500 rounded-lg border absolute top-0 right-1 cursor-pointer"
+            >
+                <span className="text-gray-700 font-semibold">Share</span>
+            </button>
+            </div>
+        </div>
+
+        </div>
+
+        <div className="relative max-w-full px-32 mx-auto mt-8">
+        {image ? (
+            <img
+            className="w-7/12 h-full md:h-56 mt-8 mx-2 object-cover"
+            src={image}
+            alt={title}
+            ></img>
+        ) : (
+            <div></div>
+        )}
+        {description ? (
+            <>
+            <div className="text-xl text-gray-900 font-bold mx-2 mb-2 mt-8">Details</div>
+            {/* <div className="mt-4 mx-2 w-7/12 whitespace-pre-wrap"><pre className="text-gray-900 font-sans w-7/12">{description}</pre></div> */}
+            <div className="mt-4 mx-2 w-7/12 whitespace-pre-wrap">{description}</div>
+            </>
+        ) : (
+            <div className="text-l text-gray-900 mx-2">No description yet!</div>
+        )}
+        </div>
+    </div>
+    </Modal>
+  )
+};
+
+export default EventModal;
