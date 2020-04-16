45a8308bf7c9cf7ad45c9f44d66241bd70145158
khanguslee <khanguslee@gmail.com>
2020-04-16 18:55:13 +1000

    Retrieve unuploaded images to preview component

--- src/cms/preview-templates/eventsPreview.js
+++ src/cms/preview-templates/eventsPreview.js
@@ -4,9 +4,9 @@ import moment from 'moment';
 import EventCard from '../../components/EventCard';
 
 function EventsPreview(props) {
-  const { entry } = props;
+  const { entry, getAsset } = props;
   const title = entry.getIn(['data', 'title']);
-  const image = entry.getIn(['data', 'image']);
+  const image = getAsset(entry.getIn(['data', 'image']));
   const eventDateTime = entry.getIn(['data', 'eventDate']);
   const eventDate = moment(eventDateTime).format('DD/MM/YYYY');
   const eventTime = moment(eventDateTime).format('hh:mm A');
