1fba8d8df85ab7e48e2c7b267662ed0db28a41b3
khanguslee <khanguslee@gmail.com>
2020-04-28 18:31:32 +1000

    Add favicons to website

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,50 @@
 module.exports = {
   plugins: [
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `MAC Website`,
+        short_name: `MAC`,
+        start_url: `/`,
+        background_color: `#f4e46c`,
+        theme_color: `#f4e46c`,
+        display: `standalone`,
+        icon: `static/favicon/android-chrome-512x512.png`,
+        icons: [
+          {
+            src: `favicon/favicon-16x16.png`,
+            sizes: `16x16`,
+            type: `image/png`,
+          },
+          {
+            src: `favicon/favicon-32x32.png`,
+            sizes: `32x32`,
+            type: `image/png`,
+          },
+          {
+            src: `favicon/favicon-48x48.png`,
+            sizes: `48x48`,
+            type: `image/png`,
+          },
+          {
+            src: `favicon/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+          {
+            src: `favicon/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `favicon/apple-touch-icon.png`,
+            sizes: `180x180`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    {
       resolve: `gatsby-plugin-sass`,
       options: {
         postCssPlugins: [require('tailwindcss')('./tailwind.config.js')],
